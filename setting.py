f1d17cc42556945403a0ec77fc8076ac5df0f15a
Divyanshu Shinde <divyanshushinde103@gmail.com>
2024-07-23 04:58:52 +0530

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,10 @@
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'divyanshushinde103@gmail.com'
+EMAIL_HOST_PASSWORD = 'DIVYANSHU_SHINDE.103'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# server.quit()
